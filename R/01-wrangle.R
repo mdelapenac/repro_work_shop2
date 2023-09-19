93fc3a691acd1f7cba34bfddd7922c4593b9557f
mdelapenac <mdelapena@arizona.edu>
2023-09-19 05:30:43 -0700

    Initial commit

---
+++ R/01-wrangle.R
@@ -0,0 +1,29 @@
+
+#load packages -----
+library(readr)
+library(lme4)
+library(dplyr)
+
+#Read data
+gapminder <- read_csv("data/gapminder_data.csv")
+
+head(gapminder)
+str(gapminder)
+
+#wrangle the data
+gap_1980s<-filter(gapminder, year>=1980, year<1990)
+
+print("hello")
+
+print("hello")
+
+print("hello")
+
+library(usethis)
+usethis::use_git()
+
+git_sitrep()
+git_vaccinate()
+
+
+
